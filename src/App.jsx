d18b3ec7ccf854d2a54932c9cf4bbc973ffce6bd
AkashMAbrrar <madborakash48@gmail.com>
2024-11-20 12:06:37 +0600

    'this is the first commit for set up project for the very first time

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl text-center">Vite + React</h1>
+      <button className="btn btn-primary">Button</button>
+    </>
+  );
+}
+
+export default App;
